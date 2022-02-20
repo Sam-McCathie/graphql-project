69fbedcb5a39df4da7410487d43d8e9e0a572296
Sam <sammccathie2@gmail.com>
2022-02-20 20:32:50 +1300

    Testing queries

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const {graphqlHTTP} = require("express-graphql");
+const schema = require("./schema/schema.js");
+
 const app = express();
 const PORT = 9000;
 
 // middleware
 // graphqlHTTP takes an argument of a schema object.
-app.use(`/graphql`, graphqlHTTP({}));
+app.use(
+  `/graphql`,
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,15 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 // Below is object destructuring
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+// dummy data
+const books = [
+  {name: "Bookie wookie", genre: "Fantasy", id: "1"},
+  {name: "book book", genre: "Fantasy", id: "2"},
+  {name: "strawberry", genre: "Sci-Fi", id: "3"},
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -10,4 +18,23 @@ const BookType = new GraphQLObjectType({
     name: {type: GraphQLString},
     genre: {type: GraphQLString},
   }),
+});
+
+// RootQuery => how you get into the graph
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: {id: {type: GraphQLString}},
+      resolve(parent, args) {
+        // code to get data from db/ other source
+        return _.find(books, {id: args.id});
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
