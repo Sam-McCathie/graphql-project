8e505751e851ad86786228082ed235781bf898e1
Sam <sammccathie2@gmail.com>
2022-02-21 09:47:13 +1300

    Added type relationships

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,21 +2,43 @@ const graphql = require("graphql");
 const _ = require("lodash");
 
 // Below is object destructuring
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} =
+  graphql;
 
 // dummy data
 const books = [
-  {name: "Bookie wookie", genre: "Fantasy", id: "1"},
-  {name: "book book", genre: "Fantasy", id: "2"},
-  {name: "strawberry", genre: "Sci-Fi", id: "3"},
+  {name: "Bookie wookie", genre: "Fantasy", id: "1", authorID: "1"},
+  {name: "book book", genre: "Fantasy", id: "2", authorID: "1"},
+  {name: "strawberry", genre: "Sci-Fi", id: "3", authorID: "2"},
+];
+
+const authors = [
+  {name: "Sammy", age: 26, id: "1"},
+  {name: "Acacia", age: 23, id: "2"},
+  {name: "Hugo", age: 0.25, id: "3"},
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: {type: GraphQLString},
+    id: {type: GraphQLID},
     name: {type: GraphQLString},
     genre: {type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, {id: parent.authorID});
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt},
   }),
 });
 
@@ -26,10 +48,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: {id: {type: GraphQLString}},
+      args: {id: {type: GraphQLID}},
       resolve(parent, args) {
         // code to get data from db/ other source
         return _.find(books, {id: args.id});
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: {id: {type: GraphQLID}},
+      resolve(parent, args) {
+        return _.find(authors, {id: args.id});
       },
     },
   },
