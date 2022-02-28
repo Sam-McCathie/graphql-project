2a71416aad93e1fa97c2a610b12c5d33cd381132
Sam <sammccathie2@gmail.com>
2022-02-28 21:00:09 +1300

    Create react app and initial code.

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const BookList = () => {
+  return (
+    <div>
+      <h1>BookList</h1>
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
+    </div>
+  );
+};
